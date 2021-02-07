f9265010ad74ad47fe462106638b99d451e20bf6
Andy Wu <andywu0913@yahoo.com.tw>
2021-02-07 17:59:54 +0800

    Cleanup backend middlewares.

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,34 +1,40 @@
-const jwt = require(__projdir + '/utils/jwt');
+const ReturnObject = require(`${__projdir}/utils/ReturnObject`);
+
+const Errors = require(`${__projdir}/utils/Errors`);
+
+const jwtUtility = require(`${__projdir}/utils/jwtUtility`);
 
 module.exports = function(ignoreExpiration = false) {
   return async function(req, res, next) {
-    let auth = req.headers.authorization;
-
-    if(!auth) {
-      res.status(401);
-      return res.json({'successful': false, 'data': [], 'error_field': [], 'error_msg': '401 Unauthorized.'});
-    }
-
-    let token;
-    let payload;
-
+    const returnObject = new ReturnObject([]);
     try {
-      token = auth.split(' ')[1];
-      payload = await jwt.decode(token, ignoreExpiration);
-    }
-    catch(err) {
-      res.status(401);
-      return res.json({'successful': false, 'data': [], 'error_field': [], 'error_msg': 'JWT token is invalid.'});
-    }
+      const { authorization: auth } = req.headers;
 
-    if(!payload.user_id || !payload.role) {
-      res.status(500);
-      return res.json({'successful': false, 'data': [], 'error_field': ['user_id', 'role'], 'error_msg': 'JWT token contents are unrecognizable.'});
+      if (!auth) {
+        throw new Errors.ForbiddenError('401 Unauthorized.');
+      }
+
+      let token;
+      let payload;
+
+      try {
+        [, token] = auth.split(' ');
+        payload = await jwtUtility.decode(token, ignoreExpiration);
+      } catch (err) {
+        throw new Errors.ForbiddenError('JWT token is invalid.');
+      }
+
+      if (!payload.user_id || !payload.role) {
+        throw new Errors.InternalServerError('JWT token contents are unrecognizable.');
+      }
+
+      req.user_id = payload.user_id;
+      req.role = payload.role;
+
+      next();
+    } catch (err) {
+      returnObject.setError(err.message);
+      res.status(err.statusCode).json(returnObject);
     }
-
-    req.user_id = payload.user_id;
-    req.role = payload.role;
-
-    next();
   };
 };
--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,12 +1,20 @@
+const ReturnObject = require(`${__projdir}/utils/ReturnObject`);
+
+const Errors = require(`${__projdir}/utils/Errors`);
+
 module.exports = function(roles = []) {
   return function(req, res, next) {
-    let role = req.role;
+    const returnObject = new ReturnObject([]);
+    try {
+      const { role } = req;
 
-    if(!role || !roles.includes(role)) {
-      res.status(403);
-      return res.json({'successful': false, 'data': [], 'error_field': [], 'error_msg': '403 Forbidden.'});
+      if (!role || !roles.includes(role)) {
+        throw new Errors.ForbiddenError('403 Forbidden.');
+      }
+      next();
+    } catch (err) {
+      returnObject.setError(err.message);
+      res.status(err.statusCode).json(returnObject);
     }
-
-    next();
   };
 };
